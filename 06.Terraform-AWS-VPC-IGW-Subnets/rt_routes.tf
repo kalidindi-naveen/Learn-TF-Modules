c3bdc1b49d05cc19b3ab9a04d1b850aefb8f72a0
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-25 11:00:33 +0000

    Added Files

--- 06.Terraform-AWS-VPC-IGW-Subnets/rt_routes.tf
+++ 06.Terraform-AWS-VPC-IGW-Subnets/rt_routes.tf
@@ -15,3 +15,21 @@ resource "aws_route" "database-rt" {
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.this.id
 }
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[*].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[*].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "database" {
+  count          = length(var.database_subnet_cidrs)
+  subnet_id      = aws_subnet.database[*].id
+  route_table_id = aws_route_table.database.id
+}
