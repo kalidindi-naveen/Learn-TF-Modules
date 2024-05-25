c299c2da18da9627e12e9d672e2f5d9362d1f41b
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-25 09:48:34 +0000

    Added Files

--- 07.Terraform-AWS-VPC-IGW-Subnets-Test/main.tf
+++ 07.Terraform-AWS-VPC-IGW-Subnets-Test/main.tf
@@ -1,5 +1,5 @@
 module "vpc-made-easy" {
-  source              = "../04.Terraform-AWS-VPC-IGW"
+  source              = "../06.Terraform-AWS-VPC-IGW-Subnets"
   common_tags         = var.common_tags
   project_name        = var.project_name
   public_subnet_cidrs = var.public_subnet_cidrs
