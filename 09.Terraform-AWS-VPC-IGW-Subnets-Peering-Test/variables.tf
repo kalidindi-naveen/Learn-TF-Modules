2e8b04e52c5c679694a439f3fe07851f65b75779
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-28 13:06:14 +0000

    Modified CIDR

--- 09.Terraform-AWS-VPC-IGW-Subnets-Peering-Test/variables.tf
+++ 09.Terraform-AWS-VPC-IGW-Subnets-Peering-Test/variables.tf
@@ -13,15 +13,15 @@ variable "common_tags" {
 }
 
 variable "public_subnet_cidrs" {
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["100.0.1.0/24", "100.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-  default = ["10.0.11.0/24", "10.0.12.0/24"]
+  default = ["100.0.11.0/24", "100.0.12.0/24"]
 }
 
 variable "database_subnet_cidrs" {
-  default = ["10.0.21.0/24", "10.0.22.0/24"]
+  default = ["100.0.21.0/24", "100.0.22.0/24"]
 }
 
 variable "enable_peering" {
