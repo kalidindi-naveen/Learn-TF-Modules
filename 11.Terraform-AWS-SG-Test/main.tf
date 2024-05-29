dbaf3d179e15febd873fd5b564f42c8d324be7d0
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-29 02:28:19 +0000

    Added SG

--- 11.Terraform-AWS-SG-Test/main.tf
+++ 11.Terraform-AWS-SG-Test/main.tf
@@ -7,8 +7,8 @@ module "sg-made-easy" {
   project_name   = var.project_name
   environment    = var.environment
   sg_name        = var.sg_name
-  vpc_id         = data.aws_ssm_parameter.this.id
+  vpc_id         = data.aws_ssm_parameter.this.value
   sg_description = var.sg_description
   ingress        = var.ingress
-  common_tags = var.common_tags
+  common_tags    = var.common_tags
 }
