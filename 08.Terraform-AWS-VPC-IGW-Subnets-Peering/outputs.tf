6e63e954e883651444709a5f48e5afad48ca36cc
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-29 04:40:53 +0000

    Added Outputs

--- 08.Terraform-AWS-VPC-IGW-Subnets-Peering/outputs.tf
+++ 08.Terraform-AWS-VPC-IGW-Subnets-Peering/outputs.tf
@@ -5,3 +5,15 @@ output "azs_info" {
 output "vpc_id" {
   value = aws_vpc.this.id
 }
+
+output "pub_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "pri_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "db_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
