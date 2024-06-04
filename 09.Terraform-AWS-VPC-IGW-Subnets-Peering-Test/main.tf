9c812718ca45c35ff74b1bb3f2d2332319dc1975
kalidindi-naveen <step.into.iot@gmail.com>
2024-06-04 16:54:33 +0000

    Added AZ's

--- 09.Terraform-AWS-VPC-IGW-Subnets-Peering-Test/main.tf
+++ 09.Terraform-AWS-VPC-IGW-Subnets-Peering-Test/main.tf
@@ -3,6 +3,7 @@ module "vpc-made-easy" {
   source                = "git::https://github.com/kalidindi-naveen/Learn-TF-Modules.git//08.Terraform-AWS-VPC-IGW-Subnets-Peering?ref=main"
   common_tags           = var.common_tags
   project_name          = var.project_name
+  cidr_block            = var.cidr_block
   environment           = var.environment
   public_subnet_cidrs   = var.public_subnet_cidrs
   private_subnet_cidrs  = var.private_subnet_cidrs
