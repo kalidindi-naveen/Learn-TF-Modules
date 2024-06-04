aa79374a8bdfe78d8c0b90d61c7c124cb3260caf
kalidindi-naveen <step.into.iot@gmail.com>
2024-06-04 16:02:34 +0000

    Added AZ's

--- 07.Terraform-AWS-VPC-IGW-Subnets-Test/main.tf
+++ 07.Terraform-AWS-VPC-IGW-Subnets-Test/main.tf
@@ -1,6 +1,7 @@
 module "vpc-made-easy" {
   source                = "../06.Terraform-AWS-VPC-IGW-Subnets"
   common_tags           = var.common_tags
+  azs                   = var.azs
   project_name          = var.project_name
   public_subnet_cidrs   = var.public_subnet_cidrs
   private_subnet_cidrs  = var.private_subnet_cidrs
